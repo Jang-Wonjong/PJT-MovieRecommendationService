134e38d69f4cf99c4e2cbd10ff79db40317164c9
wonjong <bjlaancgk@naver.com>
2021-11-17 19:09:36 +0900

    211117 || upload pjt

--- final-pjt-back/get_movies.py
+++ final-pjt-back/get_movies.py
@@ -1,10 +1,21 @@
-import requests, json
+import requests, json, csv
 from tmdb import TMDBHelper
-
+import pandas as pd
 
 tmdb_helper = TMDBHelper('043ba4036363b1f15534a85a719d4706')
-request_url = tmdb_helper.get_request_url(language='ko-KR', page='2')
-data = requests.get(request_url).json()
+# data = []
 
-with open("data.json", "w", encoding='UTF8') as f:
-    json.dump(data, f, ensure_ascii=False)
+for i in range(1, 6):
+    request_url = tmdb_helper.get_request_url(language='ko-KR', page=str(i))
+    data = requests.get(request_url).json()
+    df = pd.json_normalize(data['results'])
+    if i == 1:
+        info = df
+    else:
+        info = info.append(df, ignore_index=True)
+    # print(type(df))
+
+info.to_csv("data.csv", header=False)
+
+# with open("data.json", "w", encoding='UTF8') as f:
+#     json.dump(data, f, ensure_ascii=False, indent=2)
--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
+    adult = models.BooleanField()
+    backdrop_path = models.TextField()
+    genre_ids = models.TextField()
+    tmdb_id = models.IntegerField()
+    original_language = models.CharField(max_length=50)
+    original_title = models.CharField(max_length=100)
     overview = models.TextField()
+    popularity = models.FloatField()
     poster_path = models.TextField()
+    release_date = models.DateField()
+    title = models.CharField(max_length=100)
+    video = models.BooleanField()
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
 
     def __str__(self):
         return self.title
