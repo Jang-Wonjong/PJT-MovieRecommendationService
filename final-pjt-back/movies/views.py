0fbf826ad0684011e55586483af26b1ab640012c
wonjong <bjlaancgk@naver.com>
2021-11-19 10:57:11 +0900

    211119

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -11,7 +11,7 @@ from .models import Movie, Review
 
 # Create your views here.
 @api_view(['GET'])
-@permission_classes([AllowAny])
+# @permission_classes([AllowAny])
 def movie_list(request):
     movies = Movie.objects.all()
     serializer = MovieListSerializer(movies, many=True)
@@ -19,7 +19,7 @@ def movie_list(request):
 
 
 @api_view(['GET'])
-@permission_classes([AllowAny])
+# @permission_classes([AllowAny])
 def movie_detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = MovieSerializer(movie)
@@ -27,17 +27,19 @@ def movie_detail(request, movie_pk):
 
 
 @api_view(['GET', 'POST'])
-@permission_classes([AllowAny])
+# @permission_classes([AllowAny])
 def review_list_or_create(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     if request.method == 'GET':
-        reviews = Review.objects.filter(moviePk=movie_pk).order_by('-pk')
+        # reviews = Review.objects.all()
+        reviews = Review.objects.filter(movie_id=movie_pk).order_by('-pk')
         # paginator = Paginator(reviews, 5)
         # page_number = request.GET.get('page_num')
         # reviews = paginator.get_page(page_number)
         serializer = ReviewSerializer(reviews, many=True)
         # data.append({'possible_page': paginator.num_pages})
         return Response(serializer.data)
+
     elif request.method == 'POST':
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
